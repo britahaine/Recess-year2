06735bc8938315d3bc51abbf495c3960d2341e0f
britahaine <britahainembabazi23@gmail.com>
2025-05-20 07:28:38 +0600

    Lists

---
+++ app.py
@@ -0,0 +1,13 @@
+
+#print("welcome to python programming")
+#print("i am britah i love mschine learning")
+#fruits =["apple","mango","orange"]
+#x,y,z=fruits
+#rint(x)
+#print(y)
+#print(z)
+
+
+names=["britah","aine","yusuf","mugoya","zaward"]
+a,b,c,d,h=names
+print(names[1])
